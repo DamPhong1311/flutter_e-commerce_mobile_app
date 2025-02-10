4dbca0fb3439a6b7dd19d1ac61d344e9bd2023af
Dam Quang Phong <phong9173123@gmail.com>
2025-02-10 23:01:54 +0700

    set up contants, theme and create splash

---
+++ lib/my_app.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+import 'utils/theme/theme.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.dartTheme,
+      home: Container(color: Colors.red,),
+    );
+  }
+}
